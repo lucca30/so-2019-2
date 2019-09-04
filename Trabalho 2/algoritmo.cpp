207bb23652274f95c164b6fb0b3968236c3f9bc4
lucca30 <lucca_martins30@yahoo.com.br>
2019-09-04 10:48:27 -0300

    PRIOc

--- Trabalho 2/algoritmo.cpp
+++ Trabalho 2/algoritmo.cpp
@@ -63,7 +63,12 @@ int get_instante_proxima_chegada(void){
 int get_instante_fim_execucao(void){
     if(AA_tarefa_em_execucao == -1) return INF;
     else{
-        if(algoritmo_atual == FCFS || algoritmo_atual == SJF || algoritmo_atual == SRTF){
+        if(
+            algoritmo_atual == FCFS || 
+            algoritmo_atual == SJF  || 
+            algoritmo_atual == SRTF ||
+            algoritmo_atual == PRIOc
+         ){
             return AA_instante_atual + AA_tempo_restante[AA_tarefa_em_execucao];
         }
         if(algoritmo_atual = RR){
@@ -182,7 +187,7 @@ void executa(void){
     }
 
     if(algoritmo_atual == SJF){
-        printf("\n\n\nInstante Atual : %d\n", AA_instante_atual);
+        printf("Instante Atual : %d\n", AA_instante_atual);
 
         for(int i=0;i<AA_instante_chegada.size();i++){
             if(AA_instante_chegada[i] == AA_instante_atual){
@@ -265,13 +270,52 @@ void executa(void){
 
 
     }
+
+    if(algoritmo_atual == PRIOc){
+        printf("Instante Atual : %d\n", AA_instante_atual);
+
+        for(int i=0;i<AA_instante_chegada.size();i++){
+            if(AA_instante_chegada[i] == AA_instante_atual){
+                AA_heap.insert(mp(-AA_prioridades[i],i));
+                printf("Adicionando %d na fila \n", i);
+            }
+
+            // atualizando o tempo de espera de cada tarefa
+            if(AA_tempo_restante[i] > 0 && AA_instante_atual > AA_instante_chegada[i] && i!= AA_tarefa_em_execucao){
+                AA_tempo_espera[i] += variacao_tempo;
+            }
+        }
+
+        if(AA_tarefa_em_execucao != -1){
+            AA_tempo_restante[AA_tarefa_em_execucao] -= variacao_tempo;  
+            printf("Reduzido o tempo da tarefa %d para %d restantes\n", AA_tarefa_em_execucao, AA_tempo_restante[AA_tarefa_em_execucao]);
+            if(AA_tempo_restante[AA_tarefa_em_execucao] == 0){
+                printf("Finalizada a tarefa %d\n", AA_tarefa_em_execucao);
+                AA_tempo_execucao[AA_tarefa_em_execucao] = AA_instante_atual - AA_instante_chegada[AA_tarefa_em_execucao];
+
+                AA_ultima_tarefa_em_execucao = AA_tarefa_em_execucao;
+                AA_tarefa_em_execucao = -1;
+            }
+        }
+        // não pode ser else, pois AA_tarefa_execução pode ter ser valor atribuído em -1 no bloco acima
+        if(AA_tarefa_em_execucao == -1 && !AA_heap.empty()){
+            AA_tarefa_em_execucao = (*AA_heap.begin()).second;
+            AA_heap.erase(AA_heap.begin());
+            printf("Escolhido a tarefa %d\n", AA_tarefa_em_execucao);
+            // verificar se houve troca de contexto
+            if(AA_ultima_tarefa_em_execucao != -1 && AA_tarefa_em_execucao != AA_ultima_tarefa_em_execucao){
+                AA_troca_contexto ++;
+            }
+        }
+
+
+    }
 }
 
 
 void inicia(void){
     // bloco condicional temporário para pular algoritmos ainda não implementados
     if(
-        algoritmo_atual == PRIOc ||
         algoritmo_atual == PRIOp ||
         algoritmo_atual == PRIOd 
     ) {
